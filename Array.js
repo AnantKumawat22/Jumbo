90567d5a68fe5c2bf41c0979ecdd5bf316d5aa20
Anant Kumawat <anantkumawat22@gmail.com>
2022-04-09 19:56:54 +0530

    first commit

--- Array.js
+++ Array.js
@@ -1,2 +1,6 @@
 var arr = ['cpp', 'python', 'java', 'php', 'csharp', 'javascript', 'dart', 'flutter',
-           'fireBase', 'html', 'css', 'jquery', ];
+           'fireBase', 'html', 'css', 'jquery', 'angular', 'reactjs', 'reactnative', 
+           'spring', 'nodejs', 'mongodb', 'mongoose', 'json', 'hibernate', 'datascience',
+           'android', 'bootstrap', 'ajax', 'mysql', 'dbms', 'oracle', 'vscode', 'turboc++', 
+           'datastructure', 'algorithm', 'array', 'string', 'msexcel', 'xml', 'scss', 'figma', 
+           'adobe', 'oops', 'websocket', 'electron', 'golang', 'bhailang'];
